bf12cc10eacdc5726b5e0682bab8c373f04d8d82
Flávio Schuindt <flavio.schuindt@gmail.com>
2013-01-31 17:40:33 -0200

    - Adiciona projeto inicial

---
+++ PML_Test2/AddinTest2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Aveva.ApplicationFramework;
+using Aveva.Pdms.Database;
+using Aveva.ApplicationFramework.Presentation;
+using Aveva.Pdms.Shared;
+using Aveva.Pdms.Utilities;
+using System.Windows.Forms;
+using System.Drawing;
+namespace PML_Test2
+{
+    //A classe deve herdar de Iaddin, pois quando o Addin for carregado no PDMS, o método Start() será executado
+    public class AddinTest2 : IAddin
+    {
+        public string Description
+        {
+            get
+            {
+                return "Um addin para teste";
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return "AddinTest";
+            }
+        }
+
+        public void Start(ServiceManager serviceManager)
+        {
+            //Criando comando
+            CommandManager commandManager = (CommandManager)serviceManager.GetService(typeof(CommandManager));
+            ComandoTeste test = new ComandoTeste();
+            commandManager.Commands.Add(test); 
+
+            //Vamos criar uma barra de comandos
+            CommandBarManager commandBarManager = (CommandBarManager)serviceManager.GetService(typeof(CommandBarManager));
+            CommandBar commandBar = commandBarManager.CommandBars.AddCommandBar("MinhaBarraDeComando");
+            
+            RootToolsCollection rtc = commandBarManager.RootTools;
+            //Criando um botão que executa o comando criado nesse addin
+            ButtonTool button = rtc.AddButtonTool("Botao", "Adicionar Nova Revisão", null, "MeuTeste");
+            button.Enabled = true;
+            button.Visible = true;
+            //Adicionando um botão à nossa barra de comandos
+            commandBar.Tools.AddTool("Botao");
+        }
+
+
+        public void Stop()
+        {
+        }
+    }
+}
---
+++ PML_Test2/ComandoTeste.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Aveva.ApplicationFramework.Presentation;
+using Aveva.Pdms.Database;
+using System.Windows.Forms;
+using Aveva.Pdms.Shared;
+
+namespace PML_Test2
+{
+    class ComandoTeste : Command
+    {
+        public ComandoTeste()
+        {
+            this.Key = "MeuTeste";
+        }
+
+        
+
+        public override void Execute()
+        {
+
+            //Ler linhas da lista de pipes
+            string[] pipes = System.IO.File.ReadAllLines(@"V:\Automacao_de_Projetos\Trabalho\Desenvolvimento\AutomacaoPDMS\Macros\lista.txt");
+
+            foreach (string pipe in pipes)
+            {
+                CreateNewRev(pipe);
+            }
+        }
+
+        private void CreateNewRev(string pipe)
+        {
+            //Selecionando o pipe
+            DbElement elem = DbElement.GetElement(pipe);
+            CurrentElement.Element = elem;
+            elem.Claim();
+
+            //Pegando a data
+            string today = DateTime.Now.ToString("dd/MM/yyyy");
+            today = today.Replace('/', '.');
+
+            string[] templateRev = { "A", "0", "1", "2" };
+
+            List<string> udas = new List<string>(); /* udas = user defined attributes*/
+            udas.Add(":NewRevi");
+            udas.Add(":Revi1");
+            udas.Add(":Revi2");
+            udas.Add(":Revi3");
+
+            List<DbAttribute> Revisao = GetUserDefinedAttributes(udas);
+
+            udas.Clear();
+            udas.Add(":NewDate");
+            udas.Add(":Date1");
+            udas.Add(":Date2");
+            udas.Add(":Date3");
+
+            List<DbAttribute> Data = GetUserDefinedAttributes(udas);
+
+            int LastRev = 0;
+            foreach (DbAttribute rev in Revisao)
+            {
+                string attrValue = elem.GetAsString(rev);
+                if (attrValue != "" && attrValue != " " && attrValue != "unset" && attrValue != "REV.")
+                {
+                    LastRev++;
+                }
+            }
+
+
+
+            if (LastRev <= Revisao.Count - 1)
+            {
+                int i = LastRev - 1;
+                for (int k = 0; k <= LastRev - 1; k++)
+                {
+                    if (k < i)
+                    {
+                        
+                        String aux = elem.GetAsString(Revisao[k]);
+                        elem.SetAttribute(Revisao[k], elem.GetAsString(Revisao[i]));
+                        elem.SetAttribute(Revisao[i], aux);
+
+                        aux = elem.GetAsString(Data[k]);
+                        elem.SetAttribute(Data[k], elem.GetAsString(Data[i]));
+                        elem.SetAttribute(Data[i], aux);
+                        i--;
+                    }
+                }
+
+                elem.SetAttribute(Revisao[LastRev], templateRev[LastRev]);
+                elem.SetAttribute(Data[LastRev], today);
+
+                i = LastRev;
+                for (int k = 0; k <= LastRev; k++)
+                {
+                    if (k < i)
+                    {
+                        String aux = elem.GetAsString(Revisao[k]);
+                        elem.SetAttribute(Revisao[k], elem.GetAsString(Revisao[i]));
+                        elem.SetAttribute(Revisao[i], aux);
+
+                        aux = elem.GetAsString(Data[k]);
+                        elem.SetAttribute(Data[k], elem.GetAsString(Data[i]));
+                        elem.SetAttribute(Data[i], aux);
+                        i--;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Todas as revisoes do pipe" + pipe + "estao preenchidas");
+            }
+
+        }
+
+        private List<DbAttribute> GetUserDefinedAttributes(List<string> attributes)
+        {
+            List<DbAttribute> udas = new List<DbAttribute>();
+            foreach (string attribute in attributes)
+            {
+                DbAttribute uda = DbAttribute.GetDbAttribute(attribute);
+                udas.Add(uda);
+            }
+
+            return udas;
+        }
+        
+    }
+}
